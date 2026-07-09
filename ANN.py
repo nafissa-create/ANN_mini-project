a1815ece418f397834f6e01280cbc8fa1e88ba04
nafissa-create <Wendpouire.Ouedraogo@strathmore.edu>
2026-07-09 11:30:44 +0300

    Delete ANN.py

--- ANN.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-import sklearn.metrics as ac
-import sklearn.model_selection as kfx
-
-data= pd.read_csv("teen_mental_health.csv")
-print(data.head())
-print("Shape:", data.shape)
-print(data.isnull().sum())
